3b3859f6f88f6e3e2f92d03392b6951bca68e667
Josh Mu <mu@joshmu.com>
2020-01-29 16:22:24 +1000

    expressions with ternary operators

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react'
 import './App.css'
 
 class App extends Component {
+  foo() {
+    return 'boo'
+  }
+
   render() {
+    const name = 'josh mu'
+    const loading = false
+    const showName = true
     return (
       <div className="App">
-        <h1>hello from react</h1>
+        {loading ? <h4>loading...</h4> : <h1>hello {showName && name}</h1>}
       </div>
     )
   }
