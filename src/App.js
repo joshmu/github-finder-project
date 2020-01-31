5106f16e9b7025682aaaa9bcf66b77f9da9246e6
Josh Mu <mu@joshmu.com>
2020-01-31 16:12:24 +1000

    added axios call

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import './App.css'
+import axios from 'axios'
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: true
+  }
+
+  async componentDidMount() {
+    const res = await axios.get('https://api.github.com/users')
+    this.setState({ users: res.data, loading: false })
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,7 +22,7 @@ class App extends Component {
           <Navbar />
         </nav>
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     )
